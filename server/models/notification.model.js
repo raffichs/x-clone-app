810e0b5601472d240b1585482d8af59edac3ba5f
raffichaesa <140777811+raffichs@users.noreply.github.com>
2024-07-16 14:03:30 +0700

    user routes and controllers

---
+++ server/models/notification.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const notificationShema = new mongoose.Schema(
+  {
+    from: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    to: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+      enum: ["follow", "like"],
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Notification = mongoose.model("Notification", notificationShema);
+
+export default Notification;
