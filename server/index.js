6b472d419d586043d034eb7081bb7aec60e6f1ce
raffichaesa <140777811+raffichs@users.noreply.github.com>
2024-07-23 00:40:34 +0700

    use cors

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 import path from "path";
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { v2 as cloudinary } from "cloudinary";
@@ -25,6 +26,7 @@ const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" })); // to parse req.body
 app.use(express.urlencoded({ extended: true })); // to parse form data (urlencoded)
+app.use(cors());
 
 app.use(cookieParser());
 
