ad45f653abe938fb2822c40a1113d254d0bbddd1
raffichaesa <140777811+raffichs@users.noreply.github.com>
2024-07-22 22:59:19 +0700

    deployment

--- server/index.js
+++ server/index.js
@@ -33,14 +33,6 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/dist")));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
-  });
-}
-
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
   connectMongoDB();
