e6caf128014bbab8174e27cc716d09bed4192bc4
raffichaesa <140777811+raffichs@users.noreply.github.com>
2024-07-22 23:48:48 +0700

    deployment

--- server/db/connectMongoDB.js
+++ server/db/connectMongoDB.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect("mongodb+srv://raffi4244:aMAjq0iOQMtlGD3v@cluster0.nk7h2qp.mongodb.net/x-db?retryWrites=true&w=majority&appName=Cluster0");
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error connection to mongoDB: ${error.message}`);
