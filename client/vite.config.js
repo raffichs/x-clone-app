420f22c19ee986b3298496215768d700ad660863
raffichaesa <140777811+raffichs@users.noreply.github.com>
2024-07-23 00:02:12 +0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: import.meta.env.API_URL_URL,
+        target: import.meta.env.VITE_API_URL,
         changeOrigin: true,
       },
     },
