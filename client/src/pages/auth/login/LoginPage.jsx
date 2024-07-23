a1d759debfb1c34108ce30690984abe63add8f5d
raffichaesa <140777811+raffichs@users.noreply.github.com>
2024-07-23 19:51:00 +0700

    Update LoginPage.jsx

--- client/src/pages/auth/login/LoginPage.jsx
+++ client/src/pages/auth/login/LoginPage.jsx
@@ -27,7 +27,7 @@ const LoginPage = () => {
         const res = await fetch(
           "https://x-clone-app-theta.vercel.app/api/auth/login",
           {
-            credentials: "include",
+            credentials: "omit",
             method: "POST",
             mode: "cors",
             headers: {
